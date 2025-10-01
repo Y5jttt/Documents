d7f5aefb066696acf671bb8b014579da9f4036a2
Ysjttt <ysjttt@126.com>
2025-10-01 20:28:24 +0800

    针对NPUcraft作本地化初始修改

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -13,50 +13,15 @@ export const zhSidebar = sidebar({
       "BasicCommands",
       "HowToAsk",
     ],
-    "/SurvivalGuide/": [
-        {
-            text: "生存服介绍",
-            icon: "material-icon-theme:minecraft",
-            link: "/SurvivalGuide/SurvivalServer/",
-            prefix: "/SurvivalGuide/SurvivalServer/",
-            collapsible: false,
-            expanded: true,
-            children: [
-                "Survival-Main",
-                "Survival-Industry",
-                "Survival-Resource"
-            ],
-        },
-        {
-            text: "经济系统",
-            icon: "noto:bank",
-            link: "/SurvivalGuide/EconomicSystem/",
-            prefix: "/SurvivalGuide/EconomicSystem/",
-            collapsible: false,
-            expanded: true,
-            children: [
-                "MakeMoney",
-                "Residence",
-                "Shop"
-            ],
-        },
-        {
-            text: "常见问题",
-            icon: "fluent-color:notebook-question-mark-20",
-            link: "/SurvivalGuide/Support/",
-            prefix: "/SurvivalGuide/Support/",
-            collapsible: false,
-            expanded: true,
-            children: [
-                "Service",
-                "Technical",
-            ],
-        },
-    ],
 
-    "/PluginIntroduction/": "structure",
-    "/ServerIntroduction/": "structure",
-    "/Chronicle/": "structure",
+    "/Introduction/": [
+      "",
+      "dipi",
+      "IMUcraftCampus",
+      "Survival",
+      "ziyuan"
+      "Zhucheng"，
+    ],
 
     "/PlayerProfiles/": "structure",
 
